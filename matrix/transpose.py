eb49fe9859b1cdb03761060ef647ba6896e82ec5
Leandro090685 <Leandro090685@github.com>
2024-02-16 12:37:42 -0300

    final commit matrix

--- matrix/transpose.py
+++ matrix/transpose.py
@@ -4,4 +4,11 @@ def transpose(matrix):
 
     new_matrix = []
 
-    
+    for col in range(cols):
+        new_row = []
+
+        for row in range(rows):
+            new_row.append(matrix[row][col])
+        new_matrix.append(new_row)
+
+    return new_matrix
