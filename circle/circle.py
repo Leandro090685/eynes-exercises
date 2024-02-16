6423ad23330c9a0640adaf9e26913fcbaebec4fb
Leandro090685 <Leandro090685@github.com>
2024-02-16 12:09:28 -0300

    final commit circle

--- circle/circle.py
+++ circle/circle.py
@@ -13,3 +13,17 @@ class Circle():
         if radius <= 0:
             raise ValueError("El radio debe ser un valor positivo y mayor que cero.")
         self._radius = radius
+
+    def get_area(self):
+        return math.pi * self._radius ** 2
+
+    def get_perimeter(self):
+        return 2 * math.pi * self._radius
+
+    def __mul__(self, n):
+        if n <= 0:
+            raise ValueError("La multiplicación solo está permitida por números positivos y mayores que cero.")
+        return Circle(self._radius * n)
+
+    def __str__(self):
+        return f"Círculo de radio {self._radius}"
