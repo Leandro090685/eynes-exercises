626a5ad60e6211bea89b3916a016b470ac09b412
Leandro090685 <Leandro090685@github.com>
2024-02-16 13:19:16 -0300

    final exercises commit details

--- simple/simple.py
+++ simple/simple.py
@@ -8,8 +8,9 @@ def simple_list():
 
         dict_entry = {"id": i + 1, "age": random.randint(1, 100)}
         list_of_dicts.append(dict_entry)
-    
+
     return list_of_dicts
+
 
 def sort_list(dicts):
     sorted_list = sorted(dicts, key=lambda x: x["age"])
